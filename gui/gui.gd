7ee0930dac7e6e3de0aeb9d746cf5dda47d950d9
Lukasz Zephyr <s-zephyr@o2.pl>
2024-12-07 15:32:10 +0100

    reset

--- gui/gui.gd
+++ gui/gui.gd
@@ -12,6 +12,17 @@ func _process(delta: float) -> void:
 	var min = int(timer / 60)
 	var sec = int(timer) % 60
 	$Gui/TimerLabel.text = "%d:%02d" % [min, sec]
+	
+	if Input.is_action_just_pressed("reset"):
+		reset()
+
+func reset():
+	timer = 0.0
+	hp = 100.0
+	score = 0
+	player_got_hit(0) # refresh
+	enemy_died(0) # refresh
+	get_tree().reload_current_scene() 
 
 func player_got_hit(damage:float) -> void:
 	hp -= damage
